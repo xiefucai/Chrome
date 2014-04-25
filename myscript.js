a1584a0b636971618c92cf64669b364a4ca94e78
xiefucai <laozi12345@126.com>
2014-04-25 14:35:05 +0800

    commit the source code

---
+++ myscript.js
@@ -0,0 +1,35 @@
+﻿function _getImagesForQshare(){
+	var imgs=(Array.prototype.slice.call(document.images)).sort(function(m1,m2){
+	    return m2.width-m1.width;
+	}),imgarr=[],simgarr=[];
+	for (var i in imgs){
+		if (!imgs[i].src){
+			continue;
+		}
+		if (imgs[i].width<180||imgs[i].height<180){
+			simgarr.push(encodeURIComponent(imgs[i].src));
+			continue;
+		}
+		imgarr.push(encodeURIComponent(imgs[i].src));
+	}
+	console.log(imgarr);
+	return imgarr.length && imgarr.join("|") || simgarr.join("|");
+}
+
+chrome.extension.onRequest.addListener( 
+function(request, sender, sendResponse) {
+     var imgs=_getImagesForQshare();
+     sendResponse({"images":imgs,"frameURL":((frameElement&&frameElement.src)||(location.href)),"isTop":!frameElement}); 
+}); 
+
+function alert(s){
+	var notification = webkitNotifications.createNotification('32.png', '操作提示', s);
+	notification.ondisplay = function(event) {
+		setTimeout(function() {
+			event.currentTarget.cancel();
+			event.currentTarget = null;
+		},
+		2000);
+	}
+	notification.show();
+}
---
+++ qshare.js
@@ -0,0 +1,48 @@
+﻿function _share_tencent_weibo(info, tab){
+	var _u = "http://share.v.t.qq.com/index.php?c=share&a=index&url=$url$&appkey=801094433&title=$title$&pic=$pic$";
+	var _s = (info.selectionText||"").replace(/[\s\n]+/g, " ");
+	var _strmaxlen = 257 - tab.title.elength();
+	var _resultstr = _s.slice(0, (_strmaxlen - 4) >> 1);
+	if (_s.elength() > _strmaxlen) {
+		_strmaxlen = _strmaxlen - 3;
+		for (var i = _strmaxlen >> 1; i <= _strmaxlen; i++) {
+			if ((_s.slice(0, i)).tripurl().elength() >= _strmaxlen) {
+                    break;
+                }
+                else {
+                    _resultstr = _s.slice(0, i);
+                }
+            }
+            _resultstr += "...";
+        } else {
+            _resultstr = _s;
+        }
+	var url = _u.replace("$title$", encodeURIComponent((_resultstr&&(_resultstr + " ")) + tab.title+" "));
+	if (info.mediaType&&info.mediaType=="image"){
+		url = url.replace("$pic$", encodeURIComponent(info.srcUrl||""));
+		url = url.replace("$url$", encodeURIComponent(tab.url)).substr(0, 2048);
+		window.open(url);
+	}else{
+		chrome.tabs.getSelected(null, function(tab) { 
+		  chrome.tabs.sendRequest(tab.id, {}, function(response) { 
+		    url = url.replace("$pic$", response.images||"");
+		    url = url.replace("$url$", encodeURIComponent(tab.url)).substr(0, 2048);
+			window.open(url);
+		  }); 
+		});
+	}		
+}
+
+String.prototype.elength = function() {
+        return this.replace(/[^\u0000-\u00ff]/g, "aa").length;
+};
+String.prototype.tripurl = function() {
+        return this.replace(new RegExp("((news|telnet|nttp|file|http|ftp|https)://){1}(([-A-Za-z0-9]+(\\.[-A-Za-z0-9]+)*(\\.[-A-Za-z]{2,5}))|([0-9]{1,3}(\\.[0-9]{1,3}){3}))(:[0-9]*)?(/[-A-Za-z0-9_\\$\\.\\+\\!\\*\\(\\),;:@&=\\?/~\\#\\%]*)*", "gi"), new Array(12).join("aa"));
+};
+
+var menutitle = "转播到腾讯微博";
+var parent = chrome.contextMenus.create({"title": menutitle,"contexts":["selection","image"],"onclick":_share_tencent_weibo,});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+        _share_tencent_weibo({},tab);
+ });
