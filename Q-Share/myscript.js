0085ea5878e77fdd03a6bd1c85ec9234bdf47714
xiefucai <xiefucai@xunlei.com>
2021-06-07 10:53:49 +0800

    add: send2reader

--- Q-Share/myscript.js
+++ Q-Share/myscript.js
@@ -1,12 +1,12 @@
-﻿function _getImagesForQshare(){
-	var imgs=(Array.prototype.slice.call(document.images)).sort(function(m1,m2){
-	    return m2.width-m1.width;
-	}),imgarr=[],simgarr=[];
-	for (var i in imgs){
-		if (!imgs[i].src){
+﻿function _getImagesForQshare () {
+	var imgs = (Array.prototype.slice.call(document.images)).sort(function (m1, m2) {
+		return m2.width - m1.width;
+	}), imgarr = [], simgarr = [];
+	for (var i in imgs) {
+		if (!imgs[i].src) {
 			continue;
 		}
-		if (imgs[i].width<180||imgs[i].height<180){
+		if (imgs[i].width < 180 || imgs[i].height < 180) {
 			simgarr.push(encodeURIComponent(imgs[i].src));
 			continue;
 		}
@@ -15,20 +15,21 @@
 	return imgarr.length && imgarr.join("|") || simgarr.join("|");
 }
 
-chrome.extension.onRequest.addListener( 
-function(request, sender, sendResponse) {
-     var imgs=_getImagesForQshare();
-     sendResponse({'images':imgs,'frameURL':((frameElement&&frameElement.src)||(location.href)),'isTop':!frameElement}); 
-}); 
+function giveMeAllImages (request, sender, sendResponse) {
+	var imgs = _getImagesForQshare();
+	sendResponse({ 'images': imgs, 'frameURL': ((frameElement && frameElement.src) || (location.href)), 'isTop': !frameElement });
+}
 
-function alert(s){
+chrome.extension.onRequest.addListener(giveMeAllImages);
+
+function alert (s) {
 	var notification = webkitNotifications.createNotification('32.png', '操作提示', s);
-	notification.ondisplay = function(event) {
-		setTimeout(function() {
+	notification.ondisplay = function (event) {
+		setTimeout(function () {
 			event.currentTarget.cancel();
 			event.currentTarget = null;
 		},
-		2000);
+			2000);
 	}
 	notification.show();
 }
