9921289b2aa6cb998943312cc7da139acaec28f6
xiefucai <xiefucai@xunlei.com>
2021-11-25 16:09:21 +0800

    -

--- vue3-chrome-ext/src/data/feedStatus.ts
+++
@@ -1,5 +0,0 @@
-export enum FeedStatus {
-  INITED = 0,
-  SUCCESS = 1,
-  FAILED = 2
-}
--- vue3-chrome-ext/src/pages/popup/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './Index.vue'
-import '@/assets/css/tailwind.css'
-import './index.css'
-createApp(App).mount('#app')
